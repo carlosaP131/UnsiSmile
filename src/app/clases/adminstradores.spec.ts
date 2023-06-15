dcdb633dc73e1810d55daa340cb70d691a4dfa16
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-15 16:58:13 -0600

    Creacion de clases

---
+++ src/app/clases/adminstradores.spec.ts
@@ -0,0 +1,7 @@
+import { Adminstradores } from './adminstradores';
+
+describe('Adminstradores', () => {
+  it('should create an instance', () => {
+    expect(new Adminstradores()).toBeTruthy();
+  });
+});
