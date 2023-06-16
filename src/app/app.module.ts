a6de01f5ef8ea7fc2f97eee5e8a6653690cd4d94
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-16 16:39:38 -0600

    rutas cradas junto con el html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CatedraticosComponent } from './componente/catedraticos/catedraticos.component';
 import { AlumnosComponent } from './componente/alumnos/alumnos.component';
 import { AdministradoresComponent } from './componente/administradores/administradores.component';
+import { HomeComponent } from './componente/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatedraticosComponent,
     AlumnosComponent,
-    AdministradoresComponent
+    AdministradoresComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/clases/catedraticos.ts
+++ src/app/clases/catedraticos.ts
@@ -1,2 +1,14 @@
 export class Catedratico {
+    idCatedratico : Number = 0 
+    nombre: String = ""
+    nombre2: String = " " 
+    apellido:String = " " 
+    apellido2:String = " " 
+    curp:String = " " 
+    telefono:String = " " 
+    sexo: String = " "//cambiar a char 0
+    fechaNacimiento: Date= new Date
+    numeroTrabajador: String = " "
+    emailCatedratico: String = " "
+    estatus: boolean= true
 }
