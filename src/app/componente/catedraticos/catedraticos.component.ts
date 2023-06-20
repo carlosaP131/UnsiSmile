f6f631092a719da2e16877cae4c8ff0648c69e87
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-20 17:02:55 -0600

    Botones implementados

--- src/app/componente/catedraticos/catedraticos.component.ts
+++ src/app/componente/catedraticos/catedraticos.component.ts
@@ -25,7 +25,7 @@ export class CatedraticosComponent implements OnInit{
   }
 
   getCatedraticos(): void {
-    console.log('ingresnado')
+    console.log('ingresnado==============>>>>>>>>>>><')
     this.service.getCatedraticos()
     .subscribe(
       data =>{
