5ec9ec7cf230bc692350e380133bf3a1cbc76e49
Tu Nombre <you@example.com>
2023-06-20 16:05:03 -0600

    Administradores

--- src/app/clases/adminstradores.ts
+++ src/app/clases/adminstradores.ts
@@ -1,2 +1,14 @@
-export class Adminstradores {
+export class Administradores {
+    idAdministrador: Number = 0
+    nombre1:String = " "
+    nombre2: String = " "
+    apellido1: String = " "
+    apellido2: String = " "
+    curp: String = " "
+    telefono: String = " "
+    numeroTrabajador: String = " "
+    fechaNacimiento: String = " "
+    sexo: String = " "
+    emailAdmin: String = " "
+    estatus: String = " "
 }
