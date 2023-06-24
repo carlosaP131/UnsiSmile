2f1170c929dfce683eb7ab6de1a48690dc4a93ed
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-24 16:44:49 -0600

    Comentado componentes.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+/**
+ * Autores: Carlos Aurelio Alcantara Perez
+ * Fecha de modificacion: 15/06/23
+ * Comentarios: Componentes de la paguina principal
+ */
 import { Component } from '@angular/core';
 
 @Component({
