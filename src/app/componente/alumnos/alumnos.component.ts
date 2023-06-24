48afe050dcc3147d26146baad599d327d8a6c4d2
Getze1221 <alebk201@gmail.com>
2023-06-24 16:49:20 -0600

    cometarios

--- src/app/componente/alumnos/alumnos.component.ts
+++ src/app/componente/alumnos/alumnos.component.ts
@@ -1,26 +1,39 @@
+/**
+ *Autores:Gonzalez Cruz Getzemani Alejandro 
+ *        Jesus Hernandez Vite 
+ *Fecha de creación:15 de junio del 2023
+ *Fecha de Modificación:24 de junio 2023 
+ *Descripcion: css de vista alumnos
+ */
+
+// Importaciones necesarias
 import { Component, OnInit } from '@angular/core';
 import { Alumnos } from 'src/app/clases/alumnos';
 import { AlumnosService } from 'src/app/fservice/alumnos.service';
 
+// Decorador del componente
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['./alumnos.component.css']
 })
+
 export class AlumnosComponent implements OnInit {
-  listaALumnos: Alumnos[] = []
-  alumnos = new Alumnos()
-  listaSexos: string[] = ["M", "F"]
-  listaSemestres: String[] = ["113", "213", "313"]
-  listaGrupos: String[] = ["A", "B", "C"]
+  // Declaración de variables
+  listaALumnos: Alumnos[] = [] // Lista de alumnos
+  alumnos = new Alumnos() // Objeto para almacenar los datos de un alumno nuevo
+  listaSexos: string[] = ["M", "F"] // Lista de opciones para el campo "sexo"
+  listaSemestres: String[] = ["113", "213", "313"] // Lista de opciones para el campo "semestre"
+  listaGrupos: String[] = ["A", "B", "C"] // Lista de opciones para el campo "grupo" 
 
-
+  // Constructor del componente
   constructor(private service: AlumnosService) { }
 
+  // Método que se ejecuta al inicializar el componente
   ngOnInit(): void {
     this.getAllAlumno();
   }
-
+  // Método para obtener todos los alumnos
   getAllAlumno(): void {
     this.service.getAllAlumno()
       .subscribe(
@@ -33,7 +46,7 @@ export class AlumnosComponent implements OnInit {
         }
       )
   }
-
+  // Método para eliminar un alumno  
   delete(id_alumnoAux: Number) {
     this.service.deleteAlumno(id_alumnoAux)
       .subscribe(() => {
@@ -41,7 +54,7 @@ export class AlumnosComponent implements OnInit {
 
       });
   }
-
+  // Método para crear un alumno
   create(): void {
     this.service.createAlumno(this.alumnos).subscribe({
       next: (res) => {
@@ -52,15 +65,15 @@ export class AlumnosComponent implements OnInit {
 
     });
   }
+  // Método para actualizar un alumno (preparación)
+  updateAlumno(alumnos: Alumnos): void {
+    this.alumnos = alumnos
 
-  updateAlumno(alumnos:Alumnos): void{
-     this.alumnos = alumnos
+    console.log(this.alumnos)
 
-     console.log(this.alumnos)
-    
   }
-
-  update2():void{
+  // Método para actualizar un alumno (confirmación)
+  update2(): void {
     this.service.updateAlumno(this.alumnos.idAlumno, this.alumnos).subscribe({
       next: (res) => {
         this.getAllAlumno()
@@ -70,12 +83,6 @@ export class AlumnosComponent implements OnInit {
 
     });
   }
-
-
-
-
-
-
 }
 
 
--- src/app/fservice/alumnos.service.ts
+++ src/app/fservice/alumnos.service.ts
@@ -1,7 +1,17 @@
+/**
+ *Autores:Gonzalez Cruz Getzemani Alejandro 
+ *        Jesus Hernandez Vite 
+ *Fecha de creación:15 de junio del 2023
+ *Fecha de Modificación:24 de junio 2023 
+ *Descripcion: css de vista alumnos
+ */
+
+// Importaciones necesarias
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+// Decorador Injectable para indicar que esta clase es un servicio
 @Injectable({
   providedIn: 'root'
 })
@@ -11,29 +21,30 @@ export class AlumnosService {
   private baseUrl1 = 'http://localhost:8080/unsis/';
 
   //pasar el HttpClient como parametro en el contructor
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
+  // Método para obtener todos los alumnos
   getAllAlumno(): Observable<any> {
-    return this.http.get(`${this.baseUrl1}`+ 'obtenerTodosAlumnos');
+    return this.http.get(`${this.baseUrl1}` + 'obtenerTodosAlumnos');
   }
 
-  createAlumno(alumno: object): Observable<object>{
-    return this.http.post(`${this.baseUrl1}`+ 'crearAlumno', alumno);
+  // Método para crear un alumno
+  createAlumno(alumno: object): Observable<object> {
+    return this.http.post(`${this.baseUrl1}` + 'crearAlumno', alumno);
   }
 
-  deleteAlumno(id_alumnoAux: Number): Observable<any>{
-    return this.http.delete(`${this.baseUrl1}eliminarAlumno/${id_alumnoAux}`, {responseType: 'text'});
+  // Método para eliminar un alumno
+  deleteAlumno(id_alumnoAux: Number): Observable<any> {
+    return this.http.delete(`${this.baseUrl1}eliminarAlumno/${id_alumnoAux}`, { responseType: 'text' });
   }
 
-  getAlumno(id: Number): Observable<Object>{
+  // Método para obtener un alumno por su ID
+  getAlumno(id: Number): Observable<Object> {
     return this.http.get(`${this.baseUrl1}/obtenerAlumnos/${id}`);
   }
 
-  updateAlumno(id: Number, value: any): Observable<Object>{
+  // Método para actualizar un alumno
+  updateAlumno(id: Number, value: any): Observable<Object> {
     return this.http.put(`${this.baseUrl1}editarAlumnos/${id}`, value);
   }
-
-
-  
 }
