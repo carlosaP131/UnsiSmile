864f998df9364fbe15dc6a9ea950bedf085bcc41
Getze1221 <alebk201@gmail.com>
2023-06-27 16:45:58 -0600

    cambios

--- src/app/clases/alumnos.ts
+++ src/app/clases/alumnos.ts
@@ -6,8 +6,6 @@
  */
 export class Alumnos {
 
-    //si es necesario utilizar paciente alumno
-    //si es necesario utilizar id_semestre_grupo,id_usuario
     idAlumno: Number = 0
     nombre: String = ""
     nombre2: String = ""
