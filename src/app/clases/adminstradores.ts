b2e7c59690973af0d47ef2083a6f5817df1aee62
elioenai46 <elio.antonio.654@gmail.com>
2023-06-27 16:41:46 -0600

    Finalizado

--- src/app/clases/adminstradores.ts
+++ src/app/clases/adminstradores.ts
@@ -6,15 +6,15 @@
  */
 export class Administradores {
     idAdministrador: Number = 0
-    nombre1:String = " "
-    nombre2: String = " "
-    apellido1: String = " "
-    apellido2: String = " "
-    curp: String = " "
+    nombre1:String = ""
+    nombre2: String = ""
+    apellido1: String = ""
+    apellido2: String = ""
+    curp: String = ""
     telefono: String = " "
-    numeroTrabajador: String = " "
-    fechaNacimiento: String = " "
-    sexo: String = " "
-    emailAdmin: String = " "
-    estatus: String = " "
+    numeroTrabajador: String = ""
+    fechaNacimiento: String = ""
+    sexo: String = ""
+    emailAdmin: String = ""
+    estatus: String = ""
 }
